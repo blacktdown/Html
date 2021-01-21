00625ccf9ba4b1cf2ec23245520afdbc59b173d0
traver9044@gmail.com <traver9044@gmail.com>
2021-01-21 18:07:02 +0900

    2021/01/21 포토폴리오 작성중

---
+++ Jboard/Js/gnb.js
@@ -0,0 +1,12 @@
+$(function(){
+    var gnb = $('.gnb > li > a');
+    gnb.click(function(e){
+        e.preventDefault();
+        var isOpen = $(this).next().is(':visible');
+        if(isOpen){
+            $(this).next().slideUp(200);
+        }else{
+            $(this).next().Down(200);
+        }
+    });
+});
